81b1ad4805264f90b2cf39335b4d2e32eaa9e036
Kiment <kiment2002@gmail.com>
2023-10-31 14:45:11 +0100

    manque que la liste

--- cafemamie/src/Controller/BaseController.php
+++ cafemamie/src/Controller/BaseController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\AjoutCafeType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\TypeCafe;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BaseController extends AbstractController
 {
@@ -12,5 +16,31 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig', []);
+    }
+
+    #[Route('/ajoutcafe', name: 'app_ajoutcafe')]
+    public function ajoutCafe(Request $request, EntityManagerInterface $em): Response
+    {
+        $typecafe = new TypeCafe();
+
+        $form = $this->createForm(AjoutCafeType::class, $typecafe);
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($typecafe);
+                $em->flush();
+                $this->addFlash('notice', 'Café ajouté !');
+                return $this->redirectToRoute('app_ajoutcafe');
+            }
+        }
+        return $this->render('base/ajoutcafe.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/liste-typecafe', name: 'app_liste-typecafe')]
+    public function listeTypecafe(): Response
+    {
+        return $this->render('base/liste-typecafe.html.twig', []);
     }
 }
---
+++ cafemamie/src/Entity/TypeCafe.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TypeCafeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TypeCafeRepository::class)]
+class TypeCafe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $designation = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $origine = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): static
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getOrigine(): ?string
+    {
+        return $this->origine;
+    }
+
+    public function setOrigine(string $origine): static
+    {
+        $this->origine = $origine;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
